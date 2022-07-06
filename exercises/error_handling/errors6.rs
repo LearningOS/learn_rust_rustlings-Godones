5105977d74041fdc6833794bcb9016ca798a3684
Godones <1925466036@qq.com>
2022-07-06 18:44:05 +0800

    update

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,7 +8,7 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
@@ -20,7 +20,7 @@ enum ParsePosNonzeroError {
 }
 
 impl ParsePosNonzeroError {
-    // TODO: add another error conversion function here.
+
 }
 
 fn parse_pos_nonzero(s: &str)
@@ -28,9 +28,15 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+    let x = s.parse::<i64>();
+    let x = match x {
+        Ok(x) => PositiveNonzeroInteger::new(x),
+        Err(e) => return Err(ParsePosNonzeroError::ParseInt(e))
+    };
+    match x {
+        Ok(x) => Ok(x),
+        Err(e) => Err(ParsePosNonzeroError::Creation(e))
+    }
 }
 
 // Don't change anything below this line.
--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,12 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+
+macro_rules! my_macro {
+    ($str:expr) => {
+        "Hello".to_string() + " " + ($str)
+    };
+}
 
 #[cfg(test)]
 mod tests {
