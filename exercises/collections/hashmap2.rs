98ab41700159318fe5758d628a3f5f61c71b8411
Godones <1925466036@qq.com>
2022-07-06 14:57:18 +0800

    update

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -12,7 +12,7 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -23,6 +23,17 @@ enum Fruit {
     Mango,
     Lychee,
     Pineapple,
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    k,
 }
 
 fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
@@ -32,12 +43,28 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         Fruit::Mango,
         Fruit::Lychee,
         Fruit::Pineapple,
+        Fruit::a,
+        Fruit::b,
+        Fruit::c,
+        Fruit::d,
+        Fruit::e,
+        Fruit::f,
+        Fruit::g,
+        Fruit::h,
+        Fruit::i,
+        Fruit::j,
+        Fruit::k,
     ];
 
     for fruit in fruit_kinds {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        if basket.contains_key(&fruit) {
+            continue;
+        } else {
+            basket.insert(fruit, 4);
+        }
     }
 }
 
